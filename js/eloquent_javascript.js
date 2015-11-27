7f6e0299d746d1a62cf0cbc5d8bf23edf4ba51bc
stackcats <646035537@qq.com>
2015-11-27 17:57:40 +0800

    chapter 2

--- js/eloquent_javascript.js
+++ js/eloquent_javascript.js
@@ -1,10 +1,8 @@
 /*
  * Eloquent JavaScript
  */
-var assert = require('assert');
 
 module.exports = {
-    loopTriangle: loopTriangle,
     minimun: minimun,
     isEven: isEven,
     countChar: countChar,
@@ -27,6 +25,46 @@ function loopTriangle(n) {
 	console.log(produceSharp(i));
     }
 }
+
+loopTriangle(7);
+
+//FizzBuzz
+function fizzbuzz() {
+    
+    for(i = 1; i <= 100; i++) {
+	if(i % 3 == 0 && i % 5 == 0)
+	    console.log("FizzBuzz");
+	else if(i % 3 == 0)
+	    console.log("Fizz");
+	else if(i % 5 == 0)
+	    console.log("Buzz");
+	else
+	    console.log(i);
+    }
+}
+
+fizzbuzz();
+
+//Chess board
+function chessBoard(n) {
+    function row(col) {
+	var s = '';
+	var pattern = null;
+	if(col % 2 == 0)
+	    pattern = '# ';
+	else
+	    pattern = ' #';
+	for(var i = 0; i < n; i += 2) {
+	    s += pattern;
+	}
+	return s;
+    }
+    for(var i = 0; i < n; i++) {
+	console.log(row(i));
+    }
+}
+
+chessBoard(8);
 
 /*
  *chapter 3 Function
