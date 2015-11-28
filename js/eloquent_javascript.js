dcb09dd1636f5f293fd1da243cc61602a6989517
stackcats <646035537@qq.com>
2015-11-28 23:41:19 +0800

    ignore tern_port

--- js/eloquent_javascript.js
+++ js/eloquent_javascript.js
@@ -2,16 +2,9 @@
  * Eloquent JavaScript
  */
 
-module.exports = {
-    minimun: minimun,
-    isEven: isEven,
-    countChar: countChar,
-    countBs: countBs,
-};
 
-/*
- *chapter 2 Program Structure
- */
+//chapter 2 Program Structure
+
 
 //Looping a triangle
 function loopTriangle(n) {
@@ -66,14 +59,16 @@ function chessBoard(n) {
 
 chessBoard(8);
 
-/*
- *chapter 3 Function
- */
+
+//chapter 3 Function
+
 
 //Minimum
 function minimun(a, b) {
     return a < b ? a : b;
 }
+
+exports.minimun = minimun;
 
 //Recursion
 function isEven(n) {
@@ -86,6 +81,8 @@ function isEven(n) {
     return isEven(n - 2);
 }
 
+exports.isEven = isEven;
+
 //Bean counting
 function countChar(str, ch) {
     var cts = 0;
@@ -97,6 +94,159 @@ function countChar(str, ch) {
     return cts;
 }
 
+exports.countChar = countChar;
+
 function countBs(str) {
     return countChar(str, "B");
 }
+
+exports.countBs = countBs;
+
+
+//chapter 4 Data Structures: Objects and Arrays
+
+
+//The sum of a range
+function range(start, end, step) {
+    var st = step || 1;
+
+    function isEnd(a, b) {
+	if(st < 0)
+	    return a >= b;
+	else
+	    return a <= b;
+    }
+
+    var arr = [];
+
+    for(var i = start; isEnd(i, end); i += st) {
+	arr.push(i);
+    }
+
+    return arr;
+}
+
+exports.range = range;
+
+function sum(arr) {
+    var ans = 0;
+    for(var i = 0; i < arr.length; i++)
+	ans += arr[i];
+    return ans;
+}
+
+exports.sum = sum;
+
+//Reversing an array
+function reverseArray(arr) {
+    var a = [];
+    for(var i = 0; i < arr.length; i++)
+	a.unshift(arr[i]);
+    return a;
+}
+
+exports.reverseArray = reverseArray;
+
+function reverseArrayInPlace(arr) {
+    
+    for(var i = 0, j = arr.length - 1; i < j; i++, j--){
+	var tmp = arr[i];
+	arr[i] = arr[j];
+	arr[j] = tmp;
+    }
+
+}
+
+exports.reverseArrayInPlace = reverseArrayInPlace;
+
+//A List
+
+function Node(v, rest) {
+    this.value = v;
+    this.rest = rest;
+}
+
+Node.prototype.toString = function() {
+    return String(this.value);
+};
+
+function List() {
+    this.head = null;
+}
+
+List.prototype.prepend = function(v) {
+    var n = new Node(v, this.head);
+    this.head = n;
+    return this.head;
+};
+
+List.prototype.toString = function() {
+    var str = "( ";
+    var lst = this.head;
+    while(lst) {
+	str += lst.value + " ";
+	lst = lst.rest;
+    }
+    str += ")";
+    return str;
+};
+
+exports.List = List;
+
+function nth(lst, n) {
+    if(n < 0)
+	return undefined;
+
+    var l = lst.head;
+    while(l && n > 0) {
+	l = l.rest;
+	n--;
+    }
+
+    if(l)
+	return l.value;
+    else
+	return undefined;
+};
+
+exports.nth = nth;
+
+function nthr(lst, n) {
+    //recursion version
+    function helper(lst, n) {
+	if(n < 0 || !lst)
+	    return undefined;
+	if(n == 0)
+	    return lst.value;
+	else
+	    return helper(lst.rest, n-1);    
+    }
+
+    return helper(lst.head, n);
+}
+
+exports.nthr = nthr;
+
+function arrayToList(arr) {
+    var lst = new List();
+    for(var i = 0; i < arr.length; i++)
+	lst.prepend(arr[i]);
+    return lst;
+}
+
+exports.arrayToList = arrayToList;
+
+function listToArray(lst) {
+    var arr = [];
+    var l = lst.head;
+    while(l) {
+	arr.unshift(l.value);
+	l = l.rest;
+    }
+    return arr;
+}
+
+exports.listToArray = listToArray;
+
+
+//chapter 5  Higher-Order Functions
--- js/test/eloquent_javascript.js
+++ js/test/eloquent_javascript.js
@@ -51,5 +51,129 @@ suite('eloquent javascript', function() {
 	    assert.equal(2, eloquent.countBs("Beans Beaf"));
 	});
     });
+
+    suite("range", function() {
+
+	test("range(1, 10) return [1,2,3,4,5,6,7,8,9,10]", function() {
+	    var arr = eloquent.range(1, 10);
+	    var tmp = [1,2,3,4,5,6,7,8,9,10];
+	    assert.deepEqual(arr, tmp);
+	});
+
+	test("range(1, 10, 2) return [1,3,5,7,9]", function() {
+	    var arr = eloquent.range(1, 10, 2);
+	    var tmp = [1,3,5,7,9];
+	    assert.deepEqual(arr, tmp);
+
+	});
+
+	test("range(10, 1, -2) return [10,8,6,4,2]", function() {
+	    var arr = eloquent.range(10, 1, -2);
+	    var tmp = [10,8,6,4,2];
+	    assert.deepEqual(arr, tmp);
+
+	});
+
+	test("range(10, 1, 2) return []", function() {
+	    var arr = eloquent.range(10, 1, 2);
+	    var tmp = [];
+	    assert.deepEqual(arr, tmp);
+
+	});
+
+	test("range(1, 10, -2) return []", function() {
+	    var arr = eloquent.range(1, 10, -2);
+	    var tmp = [];
+	    assert.deepEqual(arr, tmp);
+
+	});
+
+    });
+
+    suite("sum", function() {
+
+	test("sum(range(1,10)) return 55", function() {
+	    assert.equal(55, eloquent.sum(eloquent.range(1, 10)));
+	});
+
+	test("sum(range(1,10,-1)) return 0", function() {
+	    assert.equal(0, eloquent.sum(eloquent.range(1, 10, -1)));
+	});
+	
+    });
+
+    suite("reversearray", function() {
+
+	test("reverseArray([1,3,5]) return [5,3,1]", function() {
+	    var arr = [1,3,5];
+	    assert.deepEqual([5,3,1], eloquent.reverseArray(arr));
+	    assert.deepEqual([1,3,5], arr);
+	});
+
+	test("reverseArray([]) return []", function() {
+	    assert.deepEqual([], eloquent.reverseArray([]));
+	});
+
+	test("reverseArrayInPlace([1,3,5]) modify arr to [5,3,1]", function() {
+	    var arr = [1,3,5];
+	    eloquent.reverseArrayInPlace(arr);
+	    assert.deepEqual([5,3,1], arr);
+	});
+
+	test("reverseArrayInPlace([1,2,3,4]) modify arr to [4,3,2,1]", function() {
+	    var arr = [1,2,3,4];
+	    eloquent.reverseArrayInPlace(arr);
+	    assert.deepEqual([4,3,2,1], arr);
+	});
+
+	test("reverseArrayInPlace([]) do nothing", function() {
+	    var arr = [];
+	    eloquent.reverseArrayInPlace(arr);
+	    assert.deepEqual([], arr);
+	});
+
+
+    });
+
+    suite("A List", function() {
+
+	test("arrayToList([1,2,3])", function() {
+	    var l = eloquent.arrayToList([1,2,3]);
+	    assert.equal("( 3 2 1 )", String(l));
+	});
+
+	test("arrayToList([])", function() {
+	    var l = eloquent.arrayToList([]);
+	    assert.equal("( )", String(l));
+	});
+
+	test("listToArray", function() {
+	    var l = eloquent.arrayToList([1,2,3]);
+	    
+	    var a = eloquent.listToArray(l);
+	    assert.deepEqual([1,2,3], a);
+	});
+
+	test("nth", function() {
+	    var l = eloquent.arrayToList([1,2,3]);
+	    assert.equal(3, eloquent.nth(l, 0));
+	    assert.equal(1, eloquent.nth(l, 2));
+	    assert.equal(2, eloquent.nth(l, 1));
+	    assert.ok(!eloquent.nth(l, -1));
+	    assert.ok(!eloquent.nth(l, 10));
+	});
+
+	test("nthr", function() {
+	    var l = eloquent.arrayToList([1,2,3]);
+	    assert.equal(3, eloquent.nthr(l, 0));
+	    assert.equal(1, eloquent.nthr(l, 2));
+	    assert.equal(2, eloquent.nthr(l, 1));
+	    assert.ok(!eloquent.nthr(l, -1));
+	    assert.ok(!eloquent.nthr(l, 10));
+	});
+
+    });
+
+
 });
 
