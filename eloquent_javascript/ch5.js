3f27d2ff1104fde2a088b91bc03cac4024ec8038
stackcats <646035537@qq.com>
2015-11-30 20:38:52 +0800

    chapter 5

--- eloquent_javascript/ch5.js
+++ eloquent_javascript/ch5.js
@@ -10,8 +10,52 @@ function flatten(arr) {
 exports.flatten = flatten;
 
 //Mother-child age difference
+var ancestry = JSON.parse(require('./ancestry'));
+
+function motherChild() {
+
+    var byName = {};
+    ancestry.forEach(function(person) {
+	byName[person.name] = person;
+    });
+    
+    var all = 0, num = 0;
+    ancestry.forEach(function(each) {
+	var mother = byName[each.mother];
+	if(mother) {
+	    all += each.born - mother.born;
+	    num++;
+	}
+    });
+
+    return num ? all/num : 0 ;
+}
+
+console.log(motherChild());
 
 //Historical life expectancy
+function average() {
+    obj = {};
+    ancestry.forEach(function(each) {
+	var century = Math.ceil(each.died / 100);
+	if(obj[century]){
+	    obj[century].all += each.died - each.born;
+	    obj[century].num += 1;
+	} else {
+	    obj[century] = {};
+	    obj[century].all = each.died - each.born;
+	    obj[century].num = 1;
+	}
+    });
+    
+    for(var each in obj){
+	var averagePerCentury = obj[each].all / obj[each].num;
+	obj[each] = averagePerCentury;
+    }
+    return obj;
+}
+
+console.log(average());
 
 //Every and then some
 function every(arr, f) {
