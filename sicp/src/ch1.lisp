e8f7ce31a0b3e51399070224152d3929d7e08471
stackcats <646035537@qq.com>
2015-12-14 23:28:03 +0800

    ch1

--- sicp/src/ch1.lisp
+++ sicp/src/ch1.lisp
@@ -240,6 +240,371 @@
 ;; O(logN) 每次都变为1/3
 
 ;;; Ex 1.16
+(defun fast-expt (b n)
+  (labels ((iter (b n a)
+	     (cond
+	       ((zerop n) a)
+	       ((evenp n) (iter (* b b) (/ n 2) a))
+	       (t (iter b (1- n) (* a b))))))
+    (iter b n 1)))
+
+;;; Ex 1.17
+(defun double (n) (* 2 n))
+(defun halve (n) (/ n 2))
+(defun fast-mul (a b)
+  (cond
+    ((= b 0) 0)
+    ((= b 1) a)
+    ((evenp b) (fast-mul (double a) (halve b)))
+    (t  (+ a (fast-mul a (1- b))))))
+
+;;; Ex 1.18
+(defun fast-mul-iter (a b)
+  (labels ((iter (a b n)
+	     (cond
+	       ((= a 0) n)
+	       ((evenp a) (iter (halve a) (double b) n))
+	       (t (iter (1- a) b (+ n b))))))
+    (if (or (zerop a) (zerop b))
+	0
+	(iter a b 0))))
+
+;;; Ex 1.19
+;; b1 = bp + aq, a1 = bq + aq + ap
+;; 分别求b2,a2
+
+;; b2 = b1p + a1q 代入b1,a1
+;;    = b(p^2 + q^2) + a(2pq + q^2)
+;; 所以
+;; b2 = bp'+aq' => p' = p^2 + q^2, q' = 2pq + q^2
+
+;; a2 = b1q + a1q + a1p
+;;    = b(2pq + q^2) + a(2pq + q^2) + a(p^2 + q^2)
+;; a2 = bq' + aq' + ap' => p' = p^2 + q^2, q' = 2pq + q^2
+;; a2和b2得到的p' q'一致所以当2次变换时p' = p^2 + q^2, q' = 2pq + q^2
+
+(defun fib (n)
+  (fib-iter 1 0 0 1 n))
+(defun fib-iter (a b p q count)
+  (cond
+    ((zerop count) b)
+    ((evenp count)
+     (fib-iter a
+	       b
+	       (+ (* p p) (* q q))
+	       (+ (* 2 p q) (* q q))
+	       (halve count)))
+    (t
+     (fib-iter (+ (* b q) (* a q) (* a p))
+	       (+ (* b p) (* a q))
+	       p
+	       q
+	       (1- count)))))
+
+;;; Ex 1.20
+;; (defun gcd (a b)
+;;   (if (zerop b)
+;;       a
+;;       (gcb b (mod a b))))
+;; gcd(206 40)
+;; 尝试展开normal-order
+;; (if (zerop 40)
+;;     206
+;;     (gcd 40 (mod 206 40)))
+
+;; (if (zerop 40)
+;;     206
+;;     (if (zerop (mod 206 40))
+;; 	40
+;; 	(gcd (mod 206 40) (mod 40 (mod 206 40)))))
+
+;; (if (zerop 40)
+;;     206
+;;     (if (zerop (mod 206 40))
+;; 	40
+;; 	(if (zerop (mod 40 (mod 206 40)))
+;; 	    (mod 206 40)
+;; 	    (gcd (mod 40 (mod 206 40))
+;; 		 (mod (mod 206 40) (mod 40 (mod 206 40)))))))
+
+;; (if (zerop 40)
+;;     206
+;;     (if (zerop (mod 206 40))
+;; 	40
+;; 	(if (zerop (mod 40 (mod 206 40)))
+;; 	    (mod 206 40)
+;; 	    (if (zerop (mod (mod 206 40) (mod 40 (mod 206 40))))
+;; 		(mod 40 (mod 206 40))
+;; 		(gcd (mod (mod 206 40) (mod 40 (mod 206 40)))
+;; 		     (mod (mod 40 (mod 206 40))
+;; 			  (mod (mod 206 40)
+;; 			       (mod 40 (mod 206 40)))))))))
+
+;; (if (zerop 40)
+;;     206
+;;     (if (zerop (mod 206 40))
+;; 	40
+;; 	(if (zerop (mod 40 (mod 206 40)))
+;; 	    (mod 206 40)
+;; 	    (if (zerop (mod (mod 206 40) (mod 40 (mod 206 40))))
+;; 		(mod 40 (mod 206 40))
+;; 		(if (zerop (mod (mod 40 (mod 206 40)) ;已经等于0
+;; 				(mod (mod 206 40)
+;; 				     (mod 40 (mod 206 40)))))
+;; 		    (mod (mod 206 40) (mod 40 (mod 206 40))))))))
+
+;; 因为normal-order需要全部展开后再求值, 所以只检查上面最后一个表达式一共有21个mod的计算
+
+;; 尝试展开applicative-order
+;; (gcd 206 40)
+;; (if (zerop 40)
+;;     206
+;;     (gcd 40 (mod 206 40)))
+
+;; (gcd 40 6)
+;; (if (zerop 6)
+;;     40
+;;     (gcd 6 (mod 40 6)))
+
+;; (gcd 6 4)
+;; (if (zerop 4)
+;;     6
+;;     (gcd 4 (mod 6 4)))
+
+;; (gcd 4 2)
+;; (if (zerop 2)
+;;     4
+;;     (gcd 2 (mod 4 2)))
+;; (gcd 2 0)
+;; (if (zerop 0) 2 ...)
+;; 因为applicative-order先求值再展开, 所以需要检查上面所有表达式中的mod
+;; 一共有4次mod计算
+
+;;; Ex 1.21
+(defun smallest-divisor (n)
+  (find-divisor n 2))
+
+(defun find-divisor (n test-divisor)
+  (cond
+    ((> (* test-divisor test-divisor) n) n)
+    ((zerop (mod n test-divisor)) test-divisor)
+    (t (find-divisor n (next test-divisor)))))
+;; (smallest-divisor 199)   -> 199
+;; (smallest-divisor 1999)  -> 1999
+;; (smallest-divisor 19999) -> 7
+
+;;; Ex 1.22
+;; cl可以使用time检查运行时间
+(defun prime-p (n)
+  (if (< n 2)
+      nil
+      (= n (smallest-divisor n))))
+;; 只取3个 运行速度太快 这里改成300个
+(defun search-for-primes (a b)
+  (labels ((helper (a b acc)
+	     (cond
+	       ((or (> a b) (= 300 (length acc))) (reverse acc))
+	       ((prime-p a) (helper (+ 2 a) b (cons a acc)))
+	       (t (helper (+ 2 a) b acc)))))
+    (cond
+      ((<= a 2) (cons 2 (helper 3 b nil)))
+      ((zerop (mod a 2)) (helper (1+ a) b nil))
+      (t (helper a b nil)))))
+
+;; (SEARCH-FOR-PRIMES 1000 10000)
+;; took 1 milliseconds (0.001 seconds) to run.
+;; During that period, and with 8 available CPU cores,
+;;      1 milliseconds (0.001 seconds) were spent in user mode
+;;      0 milliseconds (0.000 seconds) were spent in system mode
+;; 2,400 bytes of memory allocated.
+
+;; (SEARCH-FOR-PRIMES 10000 100000)
+;; took 2 milliseconds (0.002 seconds) to run.
+;; During that period, and with 8 available CPU cores,
+;;      2 milliseconds (0.002 seconds) were spent in user mode
+;;      0 milliseconds (0.000 seconds) were spent in system mode
+;; 2,400 bytes of memory allocated.
+
+;; (SEARCH-FOR-PRIMES 100000 1000000)
+;; took 3 milliseconds (0.003 seconds) to run.
+;; During that period, and with 8 available CPU cores,
+;;      4 milliseconds (0.004 seconds) were spent in user mode
+;;      0 milliseconds (0.000 seconds) were spent in system mode
+;; 2,400 bytes of memory allocated
+
+;; (SEARCH-FOR-PRIMES 1000000 10000000)
+;; took 11 milliseconds (0.011 seconds) to run.
+;; During that period, and with 8 available CPU cores,
+;;      11 milliseconds (0.011 seconds) were spent in user mode
+;;       0 milliseconds (0.000 seconds) were spent in system mode
+;; 2,400 bytes of memory allocated.
+
+;;; Ex 1.23
+(defun next (n)
+  (if (= n 2)
+      3
+      (+ n 2)))
+
+;; (SEARCH-FOR-PRIMES 1000 10000)
+;; took 0 milliseconds (0.000 seconds) to run.
+;; During that period, and with 8 available CPU cores,
+;;      1 milliseconds (0.001 seconds) were spent in user mode
+;;      0 milliseconds (0.000 seconds) were spent in system mode
+;; 2,400 bytes of memory allocated
+
+;; (SEARCH-FOR-PRIMES 10000 100000)
+;; took 2 milliseconds (0.002 seconds) to run.
+;; During that period, and with 8 available CPU cores,
+;;      2 milliseconds (0.002 seconds) were spent in user mode
+;;      0 milliseconds (0.000 seconds) were spent in system mode
+;; 2,400 bytes of memory allocated
+
+;; (SEARCH-FOR-PRIMES 100000 1000000)
+;; took 3 milliseconds (0.003 seconds) to run.
+;; During that period, and with 8 available CPU cores,
+;;      3 milliseconds (0.003 seconds) were spent in user mode
+;;      0 milliseconds (0.000 seconds) were spent in system mode
+;; 2,400 bytes of memory allocated.
+
+;; (SEARCH-FOR-PRIMES 1000000 10000000)
+;; took 5 milliseconds (0.005 seconds) to run.
+;; During that period, and with 8 available CPU cores,
+;;      4 milliseconds (0.004 seconds) were spent in user mode
+;;      0 milliseconds (0.000 seconds) were spent in system mode
+;;  2,400 bytes of memory allocated.
+
+;;; Ex 1.24
+;;TODO:
+
+;;; Ex 1.25
+;; 结果正确,速度不行, Alyssa的expmod对于很大的数来说先生成一个很大的幂再取余数
+;; 而原实现递归时利用了同余的概念,保持每次取余后都小于m
+
+;;; Ex 1.26
+;; 当exp为偶数时只计算一次(expmod base (/ exp 2))
+;; 而Louis却计算了2次
+;; 复杂度为O(log(n) + log(n)) = O(n)  (log(n)是以2为底)
+
+;;; Ex 1.27
+(defun expmod (base exp m)
+  (cond
+    ((= exp 0) 1)
+    ((evenp exp)
+     (mod (expt (expmod base (/ exp 2) m) 2)
+	  m))
+    (t
+     (mod (* base (expmod base (- exp 1) m))
+	  m))))
+(defun carmichael-test (n)
+  (labels ((try-it (a)
+	     (= (expmod a n n) a)))
+    (dotimes (i (1- n) t)
+      (unless (try-it (1+ i))
+	(return nil)))))
+
+;;; Ex 1.28
+;;TODO:
+
+;;; Ex 1.29
+(defun sum (term a next b)
+  (if (> a b)
+      0
+      (+ (funcall term a)
+	 (sum term (funcall next a) next b))))
+(defun simpson (f a b n)
+  (let ((h (/ (- b a) n)))
+    (labels ((term (x)
+	       (let ((each (funcall f (+ a (* x h)))))
+		 (cond
+		   ((or (zerop x) (= x n)) each)
+		   ((evenp x) (* 2 each))
+		   (t (* 4 each))))))
+      (* (/ h 3) (sum-iter #'term 0 #'1+ n)))))
+
+;;; Ex 1.30
+(defun sum-iter (term a next b)
+  (labels ((iter (a res)
+	     (if (> a b)
+		 res
+		 (iter (funcall next a)
+		       (+ (funcall term a) res)))))
+    (iter a 0)))
+
+;;; Ex 1.31
+(defun product (term a next b)
+  (if (> a b)
+      1
+      (* (funcall term a)
+	 (product term (funcall next a) next b))))
+
+(defun fac (n)
+  (product-iter #'(lambda (x) x) 1 #'1+ n))
+
+(defun sicp-pi (n)
+  (labels ((term (x)
+	     (let ((2x (* 2 x)))
+	       (/ (* 2x (+ 2x 2))
+		  (expt (+ 2x 1) 2)))))
+    (* 4 (product-iter #'term 1.0 #'1+ n))))
+
+(defun product-iter (term a next b)
+  (labels ((iter (a res)
+	     (if (> a b)
+		 res
+		 (iter (funcall next a)
+		       (* res (funcall term a))))))
+    (iter a 1)))
+
+;;; Ex 1.32
+(defun acc (combiner null-value term a next b)
+  (if (> a b)
+      null-value
+      (funcall combiner (funcall term a)
+	       (acc combiner null-value
+		    term (funcall next a) next b))))
+
+(defun sum-acc (term a next b)
+  (acc #'+ 0 term a next b))
+
+(defun product-acc (term a next b)
+  (acc-iter #'* 1 term a next b))
+
+(defun acc-iter (combiner null-value term a next b)
+  (labels ((iter (a res)
+	     (if (> a b)
+		 res
+		 (iter (funcall next a)
+		       (funcall combiner res (funcall term a))))))
+    (iter a null-value)))
+
+;;; Ex 1.33
+(defun filter-accumulate (combiner null-value term a next b p)
+  (if (> a b)
+      null-value
+      (let ((term-a (funcall term a)))
+	(if (funcall p term-a)
+	    (funcall combiner term-a
+		     (filter-accumulate combiner
+					null-value
+					term
+					(funcall next a)
+					next
+					b
+					p))
+	    (filter-accumulate combiner
+			       null-value
+			       term
+			       (funcall next a)
+			       next
+			       b
+			       p)))))
+
+(defun sum-prime (a b)
+  (filter-accumulate #'+ 0 (lambda (x) x) a #'1+ b #'prime-p))
+
+(defun sum-coprime (n)
+  (filter-accumulate #'* 1 (lambda (x) x) 1 #'1+ (1- n)
+		     (lambda (x) (= 1 (gcd x n)))))
 
 ;;; Ex 1.34
 ;; (defun f (g)
@@ -248,3 +613,191 @@
 ;; (f 2)
 ;; (2 2) -> error
 
+;;; Ex 1.35
+(defun fixed-point (f first-guess)
+  (labels ((close-enough-p (v1 v2)
+	     (< (abs (- v1 v2)) 0.00001))
+	   (try (guess)
+	     (print guess)
+	     (let ((next (funcall f guess)))
+	       (if (close-enough-p guess next)
+		   next
+		   (try next)))))
+    (try first-guess)))
+
+(fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0)
+;; 1.1 
+;; 72.476555 
+;; 1.6127319 
+;; 14.4535 
+;; 2.586267 
+;; 7.2696724 
+;; 3.4822383 
+;; 5.536501 
+;; 4.0364065 
+;; 4.9505367 
+;; 4.3187075 
+;; 4.721779 
+;; 4.4503407 
+;; 4.6268215 
+;; 4.509361 
+;; 4.58635 
+;; 4.5353723 
+;; 4.5689015 
+;; 4.546751 
+;; 4.5613422 
+;; 4.551712 
+;; 4.55806 
+;; 4.553872 
+;; 4.5566335 
+;; 4.554812 
+;; 4.556013 
+;; 4.555221 
+;; 4.555743 
+;; 4.555399 
+;; 4.555626 
+;; 4.555476 
+;; 4.555575 
+;; 4.55551 
+;; 4.5555525 
+;; 4.555525 
+;; 4.555543 
+;; 4.555531 
+;; 4.5555387
+
+(defun average (a b)
+  (/ (+ a b) 2))
+
+(fixed-point (lambda (x) (average x (/ (log 1000) (log x)))) 1.1)
+;; 1.1 
+;; 36.788277 
+;; 19.35217 
+;; 10.841831 
+;; 6.8700476 
+;; 5.227225 
+;; 4.7019606 
+;; 4.582197 
+;; 4.560134 
+;; 4.55632 
+;; 4.5556693 
+;; 4.555558 
+;; 4.555539 
+;; 4.5555363
+
+;;; Ex 1.36
+(fixed-point (lambda (x) (/ (log 1000) (log x))) 1.1)
+
+;;; Ex 1.37
+(defun cont-frac (f1 f2 k)
+  (labels ((frac (i)
+	     (let ((n (funcall f1 i))
+		   (d (funcall f2 i)))
+	       (if (= i k)
+		   (/ n d)
+		   (/ n (+ d (frac (1+ i))))))))
+    (frac 1.0)))
+
+(defun cont-frac-iter (f1 f2 k)
+  (labels ((iter (i res)
+	     (if (= i 0)
+		 res
+		 (iter (1- i)
+		       (/ (funcall f1 i)
+			  (+ (funcall f2 i) res))))))
+    (iter (1- k) (/ (funcall f1 k) (funcall f2 k)))))
+
+;;; Ex 1.38
+(+ 2 (cont-frac (lambda (i) 1.0)
+		(lambda (i)
+		  (if (= 2 (mod i 3))
+		      (/ (1+ i) 1.5)
+		      1))
+		100))
+
+;;; Ex 1.39
+(defun tan-cf (x k)
+  (cont-frac-iter (lambda (i) (if (= i 1) x (* -1 x x)))
+		  (lambda (i) (- (* 2.0 i) 1))
+		  k))
+
+;;; Ex 1.40
+(defparameter dx 0.00001)
+
+(defun newtons-method (g guess)
+  (fixed-point (newtons-transform g) guess))
+
+(defun newton-transform (g)
+  (lambda (x)
+    (- x (/ (funcall g x) (d g) x))))
+
+(defun d (g)
+  (lambda (x)
+    (/ (- (funcall g (+ x dx)) (funcall g x))
+       dx)))
+
+(defun cubic (a b c)
+  (lambda (x)
+    (+ (* x x x) (* a x x) (* b x) c)))
+
+;;; Ex 1.41
+(defun repeat-2 (f) ;double
+  (lambda (x) (funcall f (funcall f x))))
+
+;; fucking syntax
+;; (funcall (funcall (repeat-2 (repeat-2 #'repeat-2)) #'1+) 5)
+;; ->
+;; 21
+;; (repeat-2 #'repeat-2) 将repeat-2过程应用4次
+;; 再(repeat-2 (repeat-2 #'repeat-2)) 将repeat-2过程应用8次
+;; repeat-2讲1+应用2次
+;; 所以一共16次1+ 
+
+;;; Ex 1.42
+(defun compose (f g)
+  (lambda (x) (funcall f (funcall g x))))
+
+;;; Ex 1.43
+(defun repeated (f n)
+  (if (= n 1)
+      (lambda (x) (funcall f x))
+      (repeated (compose f (lambda (x) (funcall f x))) (1- n))))
+
+;;; Ex 1.44
+(defun smooth (f)
+  (lambda (x)
+    (/ (+ (funcall f x)
+	  (funcall f (+ x dx))
+	  (funcall f (- x dx)))
+       3)))
+
+;;; Ex 1.45
+(defun average-damp (f)
+  (lambda (x) (average x (funcall f x))))
+
+(defun repeated-average-damp (f n)
+  (funcall (repeated #'average-damp n) f))
+
+(defun root (n m)
+  (lambda (x)
+    (fixed-point (repeated-average-damp
+		  (lambda (y) (/ x (expt y (1- n))))
+		  m)
+		 1.0)))
+
+;;; Ex 1.46
+(defun iterative-improve (f g)
+  (lambda (x)
+    (let ((next (funcall g x)))
+      (if (funcall f x next)
+	  next
+	  (funcall (iterative-improve f g) (funcall g x))))))
+
+(defun sqrt-iteractive-improve (x)
+  (funcall (iterative-improve #'good-enough-p
+			      #'improve)
+	   1.0))
+
+(defun fixed-point-iterative-improve (f first-guess)
+  (labels ((close-enough-p (v1 v2)
+	     (< (abs (- v1 v2)) 0.00001)))
+    (funcall (iterative-improve f #'close-enough-p) first-guess)))
--- sicp/test/test_ch1.lisp
+++ sicp/test/test_ch1.lisp
@@ -20,4 +20,79 @@
     (assert-equal (f-iter 2) (f-recur 2))
   (assert-equal (f-iter 10) (f-recur 10)))
 
-(print (cc 11 '(10 5 1)))
+;;; Ex 1.16
+(define-test fast-expt
+    (dotimes (i 5)
+      (let ((n (random 100)))
+	(assert-equal (expt 3 n)
+		      (fast-expt 3 n)))))
+
+;;; Ex 1.17
+(define-test fast-mul
+    (dotimes (i 5)
+      (let ((a (random 100))
+	    (b (random 100)))
+	(assert-equal (* a b) (fast-mul a b)))))
+
+;;; Ex 1.18
+(define-test fast-mul-iter
+    (dotimes (i 5)
+      (let ((a (random 100))
+	    (b (random 100)))
+	(assert-equal (* a b) (fast-mul-iter a b)))))
+
+;;; Ex 1.19
+(define-test fib
+    (assert-equal '(1 1 2 3 5 8 13 21 34 55)
+		  (mapcar #'fib '(1 2 3 4 5 6 7 8 9 10))))
+
+;;; Ex 1.27
+(define-test carmichael-test
+    (dolist (obj '(561 1105 1729 2821 6601))
+      (assert-true (carmichael-test obj))))
+
+;;; Ex 1.30
+(define-test sum-iter
+    (dotimes (i 10)
+      (let* ((a (random 100))
+	     (b (+ a (random 100))))
+	(assert-equal (sum #'(lambda (x) x) a #'1+ b)
+		      (sum-iter #'(lambda (x) x) a #'1+ b)))))
+
+;;; Ex 1.31
+(define-test fac
+    (assert-equal (* 1 2 3 4 5 6 7 8 9 10)
+		  (fac 10)))
+
+;;; Ex 1.32
+(define-test accumulate
+    (labels ((f (x) x))
+      (assert-equal (sum-acc f 1 #'1+ 10)
+		    (sum f 1 #'1+ 10))
+      (assert-equal (product-acc f 1 #'1+ 10)
+		    (product f 1 #'1+ 10))))
+
+;;; Ex 1.33
+(define-test filter-accumulate
+    (assert-equal 1060 (sum-prime 1 100))
+  (assert-equal 189 (sum-coprime 10)))
+
+;;; Ex 1.37
+(define-test cont-frac
+    (flet ((f (x) 1.0))
+      (dotimes (i 5)
+	(let ((k (random 20)))
+	  (assert-equal (cont-frac #'f #'f k)
+			(cont-frac-iter #'f #'f k))))))
+
+;;; Ex 1.42
+(define-test compose
+    (assert-equal 49
+		  (funcall (compose (lambda (x) (* x x))
+				    #'1+)
+			   6)))
+
+;;; Ex 1.43
+(define-test repeated
+    (assert-equal 625
+		  (funcall (repeated (lambda (x) (* x x)) 2) 5)))
