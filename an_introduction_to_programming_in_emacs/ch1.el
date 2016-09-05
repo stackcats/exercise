8401665222b242f78acf92f2af4a178f8a367f43
stackcats <463642759@qq.com>
2016-09-05 10:40:42 +0800

    ex

---
+++ an_introduction_to_programming_in_emacs/ch1.el
@@ -0,0 +1,11 @@
+;;; package --- Summary
+
+;;; Commentary:
+
+;;; Code:
+;; x => *** Eval error ***  Symbol’s value as variable is void: x
+;; (x) => *** Eval error ***  Symbol’s function definition is void: x
+(setq ct 0)
+(setq ct (+ ct 2))
+(message "Hello World")
+;;; ch1.el ends here
---
+++ an_introduction_to_programming_in_emacs/ch2.el
@@ -0,0 +1,7 @@
+;;; package --- Summary
+
+;;; Commentary:
+
+;;; Code:
+
+;;; ch2.el ends here
